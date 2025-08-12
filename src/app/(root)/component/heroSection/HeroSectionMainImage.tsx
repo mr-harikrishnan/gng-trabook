6f4374b1de26aed9fea36f8ee9eb42dd71f4d7ad
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2025-08-12 17:07:49 +0530

    completed

--- src/app/(root)/component/heroSection/HeroSectionMainImage.tsx
+++ src/app/(root)/component/heroSection/HeroSectionMainImage.tsx
@@ -7,7 +7,7 @@ const HeroSectionMainImage = () => {
     return (
         <>
             <div className='w-full flex justify-center  relative '>
-                <Image height={500} width={500} src={bgFlightImage} alt='HeroSectionMainImage' className='absolute z-[1] top-[-14px] max-w-[20rem] opacity-40
+                <Image height={500} width={500} src={bgFlightImage} alt='HeroSectionMainImage' className='lg:hidden xl:block absolute z-[1] top-[-14px] max-w-[20rem] opacity-40
                    dxs:max-w-[22rem]
                    txs:max-w-[27rem] txs:top-[-17px]  
                    lg:min-w-[39rem] lg:right-[-123px] lg:top-[-44px]' />
---
+++ src/common/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <hr className='my-8 ' />
+            <div className='w-full  flex flex-col justify-between items-center lg:items-start lg:flex-row'>
+                <p className="text-[13px] text-secondary">Copyright, Trabook 2022. All rights reserved.</p>
+                <p className="text-[13px] text-secondary">Terms & Conditions</p>
+            </div>
+        </>
+
+    )
+}
+
+export default Footer
